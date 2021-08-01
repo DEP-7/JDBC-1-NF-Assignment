e604288de5ebf5158526bcdccc477b7f6080f560
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-08-01 13:42:16 +0530

    Create Main Form

--- src/lk/ijse/dep7/rdbms/controller/MainFormController.java
+++ src/lk/ijse/dep7/rdbms/controller/MainFormController.java
@@ -1,4 +1,32 @@
 package lk.ijse.dep7.rdbms.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtId;
+    public TableView tblStudents;
+    public TextField txtName;
+    public Button btnSave;
+    public TextField txtSearch;
+    public TextField txtContact;
+    public ListView lstContact;
+
+    public void btnNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAdd_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemove_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnClear_OnAction(ActionEvent actionEvent) {
+    }
 }
